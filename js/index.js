89c9df108339e5b2cdd4bede51d039058745f75e
bjornsodenberg <garonini1@gmail.com>
2021-08-26 23:01:15 +0700

    added a comment list structure and started writing js

---
+++ js/index.js
@@ -0,0 +1,10 @@
+let addUsername = document.querySelector('.username');
+let addCommenBtn = document.querySelector('.addComment');
+let addComment = document.querySelector('.comment');
+
+addCommenBtn.addEventListener('click', function(){
+  console.log(addUsername.value);
+  console.log(addComment.value);
+
+
+})
